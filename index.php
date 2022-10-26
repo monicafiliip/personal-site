af87b8f874a82e99cdd896378f7d2c4a474b200f
monicafilip <65854234+monicafiliip@users.noreply.github.com>
2022-10-26 15:58:26 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include 'header.php';?>
+
+<!-- inceput Jumbotron -->
+
+<div class="jumbotron p-0 rounded-0 mydiv" style="background:#40916c;" >
+  <!-- inceput Card content -->
+
+  <div class="card-body text-center"> <br><br><br><br>
+    <!-- Titlu -->
+
+    <b class="card-title my-4 font_meie_script" style="color:#b7e4c7;" > Mihaela Monica Filip </b> <br>
+    <!-- Button catre pagina despre-->
+
+    <a href="despre.php" class="btn btn-default btn-lg active" role="button" style="color:#52B788;"
+    aria-disabled="true" aria-pressed="false" autocomplete="off">Citește mai multe</a>
+    <br><br><br><br><br><br><br><br>
+  </div>
+</div>
+<!-- sfarsit Jumbotron -->
+
+<?php include 'footer.php';?>
